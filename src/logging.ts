2df203978008ae2d5f900ede46bd2e004bdd787d
Daniel Imfeld <daniel@imfeld.dev>
2025-04-29 09:02:30 -1000

    add logging functions

--- src/logging.ts
+++ src/logging.ts
@@ -1,4 +1,36 @@
+import type { FileSink } from 'bun';
 import { debug } from './rmfilter/utils.js';
+
+let logFile: FileSink | undefined;
+
+function openLogFile(path: string) {
+    if (logFile) return;
+    logFile = Bun.file(path).writer();
+}
+
+function closeLogFile(): Promise<void> {
+    return logFile?.end();
+}
+
+export function log(...args: any[]) {
+    console.log(...args);
+    logFile?.write(args.join(' ') + '\n');
+}
+
+export function error(...args: any[]) {
+    console.error(...args);
+    logFile?.write(args.join(' ') + '\n');
+}
+
+export function warn(...args: any[]) {
+    console.warn(...args);
+    logFile?.write(args.join(' ') + '\n');
+}
+
+export function write(data: string) {
+    process.stdout.write(data);
+    logFile?.write(data);
+}
 
 export function debugLog(...args: any[]) {
   if (debug) {
