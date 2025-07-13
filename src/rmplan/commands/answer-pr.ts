b26e3b5af7212482c71bf26ac2e9cc484a6241b8
Daniel Imfeld <daniel@imfeld.dev>
2025-07-13 13:03:50 -1000

    remove unused file

--- src/rmplan/commands/answer-pr.ts
+++
@@ -1,6 +0,0 @@
-// Command handler for 'rmplan answer-pr'
-// Addresses Pull Request (PR) review comments using an LLM
-
-export async function answerPrCommand() {
-  // TODO: Implement answer-pr command logic
-}
