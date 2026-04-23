7487a7ff16b29f83354b904567012b7538ec6742
Daniel Imfeld <daniel@imfeld.dev>
2026-04-23 12:42:21 -1000

    disable playwright for now

--- vite.config.ts
+++ vite.config.ts
@@ -42,20 +42,20 @@ export default defineConfig({
   test: {
     silent: 'passed-only',
     projects: [
-      {
-        extends: './vite.config.ts',
-        test: {
-          name: 'client',
-          browser: {
-            enabled: true,
-            provider: playwright(),
-            instances: [{ browser: 'chromium', headless: true }],
-          },
-          env: playwrightTestEnv,
-          include: ['src/**/*.svelte.e2e.{test,spec}.{js,ts}'],
-          exclude: ['src/lib/server/**'],
-        },
-      },
+      // {
+      //   extends: './vite.config.ts',
+      //   test: {
+      //     name: 'client',
+      //     browser: {
+      //       enabled: true,
+      //       provider: playwright(),
+      //       instances: [{ browser: 'chromium', headless: true }],
+      //     },
+      //     env: playwrightTestEnv,
+      //     include: ['src/**/*.svelte.e2e.{test,spec}.{js,ts}'],
+      //     exclude: ['src/lib/server/**'],
+      //   },
+      // },
 
       {
         extends: './vite.config.ts',
